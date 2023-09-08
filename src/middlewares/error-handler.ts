f6ee1db0db51f2330babf58450f1e28a293dd3d6
Sylwia <gladiola204@interia.pl>
2023-09-08 11:02:15 +0200

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -4,11 +4,12 @@ import { CustomError } from "../errors/custom-error";
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof CustomError) {
         return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-    }
-    
+    };
+
+    console.error(err);
     res.status(400).send({
         errors: [
             { message: "Something went wrong" }
-        ] 
+        ]
     })
 }
